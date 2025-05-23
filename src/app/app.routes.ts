61a543b15e5ef112c3367157c0aae7600f3ec248
Aubar48 <rociio.fressa@gmail.com>
2025-05-23 06:46:06 -0300

    add:dashboard estructure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginRegistroComponent } from './pages/login-registro/login-registro.component';
 import { QuienesSomosComponent} from './pages/quienes-somos/quienes-somos.component'
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: InicioComponent },
   { path: 'login-registro', component: LoginRegistroComponent },
   { path: 'quienes-somos', component: QuienesSomosComponent },
-
+  { path: 'dashboard', component: DashboardComponent},
 ];
